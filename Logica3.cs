1b8359ad40c6425cdea618c7d9308ae4cc84a87c
Sethejinn <154003334+sethejinn@users.noreply.github.com>
2025-01-25 11:10:19 +0100

    Create Logica3.cs

---
+++ Logica3.cs
@@ -0,0 +1,8 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        string cadena = "estudio un ciclo de desarrollo aplicaciones";
+        Console.WriteLine("Mi ciclo es {0}", cadena.Substring(20, 10));
+    }
+}
